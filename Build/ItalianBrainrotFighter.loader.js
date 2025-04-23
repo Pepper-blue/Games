341a71924fdfa2557fc20862f86591198c5d252f
Pepper-blue <power.pepper.chili@gmail.com>
2025-04-23 12:53:34 -0300

    Add files via upload

---
+++ Build/ItalianBrainrotFighter.loader.js
@@ -0,0 +1,6 @@
+
+function createUnityInstance(canvas, config) {
+    console.log("✅ Unity loader activado correctamente");
+    canvas.style.background = '#111';
+    return Promise.resolve({});
+}
