c90f40ef6f4e92b5e912be618dab641719b2b1a8
Pepper-blue <power.pepper.chili@gmail.com>
2025-04-23 12:52:13 -0300

    Delete Build directory

--- Build/ItalianBrainrotFighter.loader.js
+++
@@ -1,6 +0,0 @@
-
-function createUnityInstance(canvas, config) {
-    console.log("✅ Unity loader activado correctamente");
-    canvas.style.background = '#111';
-    return Promise.resolve({});
-}
